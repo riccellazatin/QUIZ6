03530bf5ecf9c28c933fb132320286443f22c340
Riccel Lazatin <rmlazatin@student.hau.edu.ph>
2026-03-18 01:48:44 +0800

    Finalizing Quiz

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -40,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'services',
     "corsheaders",
+    'paypal.standard.ipn',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+PAYPAL_RECEIVER_EMAIL = env('PAYPAL_RECEIVER_EMAIL')
+PAYPAL_TEST = env('PAYPAL_TEST')
